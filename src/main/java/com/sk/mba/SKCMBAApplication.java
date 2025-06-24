97591a8999de16825dad165e1f90b0e44fbab071
장우승 <changwskr@gmail.com>
2025-06-24 16:00:05 +0900

    test

--- src/main/java/com/sk/mba/SKCMBAApplication.java
+++ src/main/java/com/sk/mba/SKCMBAApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SKCMBAApplication {
 
 	public static void main(String[] args) {
+		System.out.println("aaaaaaaaaaaa");
 		SpringApplication.run(SKCMBAApplication.class, args);
 	}
 
